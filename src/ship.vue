7d9b837b98074cede1a42c90906a36e4b3a68428
Bruno Dias <bruno.r.dias@gmail.com>
2016-01-31 11:25:17 -0200

    Content and styles

---
+++ src/ship.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="ship row" transition="expand">
+      <div class="factsheet four columns">
+        <table class="u-full-width">
+          <tbody>
+            <tr>
+              <th>Class</th>
+              <td>
+                {{ class }}
+              </td>
+            </tr>
+            <tr>
+              <th>Commissioned</th>
+              <td>
+                year {{ commissioned }}
+              </td>
+            </tr>
+            <tr>
+              <th>Fate</th>
+              <td>
+                {{ fate }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="description seven columns">
+        <h2>{{ prefix }} {{ name }}</h2>
+        {{{ desc }}}
+      </div>
+    </div>
+</template>
+
+<script>
+  import Ship from "./ships.js";
+
+  export default {
+  	data () { return new Ship() },
+  }
+</script>
